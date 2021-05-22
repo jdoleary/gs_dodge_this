9bed329f8da58be93274644b0f325796735aba2f
Jordan O'Leary <jdoleary@gmail.com>
2021-05-21 22:26:52 -0400

    src: Got single image drawing!!

--- 14_immediate_draw/source/main.c
+++ 14_immediate_draw/source/main.c
@@ -34,85 +34,87 @@ void update()
 {
     if (gs_platform_key_pressed(GS_KEYCODE_ESC)) gs_engine_quit();
 
-    // Set up 2D camera for projection matrix
-    gsi_camera2D(&gsi);
+    // // Set up 2D camera for projection matrix
+    // gsi_camera2D(&gsi);
 
-    // Rect
-    gsi_rectv(&gsi, gs_v2(500.f, 50.f), gs_v2(600.f, 100.f), GS_COLOR_RED, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    gsi_rectv(&gsi, gs_v2(650.f, 50.f), gs_v2(750.f, 100.f), GS_COLOR_RED, GS_GRAPHICS_PRIMITIVE_LINES);
+    // // Rect
+    // gsi_rectv(&gsi, gs_v2(500.f, 50.f), gs_v2(600.f, 100.f), GS_COLOR_RED, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    // gsi_rectv(&gsi, gs_v2(650.f, 50.f), gs_v2(750.f, 100.f), GS_COLOR_GREEN, GS_GRAPHICS_PRIMITIVE_LINES);
 
-    // Triangle
-    gsi_trianglev(&gsi, gs_v2(50.f, 50.f), gs_v2(100.f, 100.f), gs_v2(50.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    gsi_trianglev(&gsi, gs_v2(200.f, 50.f), gs_v2(300.f, 100.f), gs_v2(200.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_LINES);
+    // // Triangle
+    // gsi_trianglev(&gsi, gs_v2(50.f, 50.f), gs_v2(100.f, 100.f), gs_v2(50.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    // gsi_trianglev(&gsi, gs_v2(200.f, 50.f), gs_v2(300.f, 100.f), gs_v2(200.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_LINES);
 
-    // Lines
-    gsi_linev(&gsi, gs_v2(50.f, 20.f), gs_v2(500.f, 20.f), gs_color(0, 255, 0, 255));
+    // // Lines
+    // gsi_linev(&gsi, gs_v2(50.f, 20.f), gs_v2(500.f, 20.f), gs_color(0, 255, 0, 255));
 
-    // Circle
-    gsi_circle(&gsi, 350.f, 170.f, 50.f, 20, 100, 150, 220, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    gsi_circle(&gsi, 250.f, 170.f, 50.f, 20, 100, 150, 220, 255, GS_GRAPHICS_PRIMITIVE_LINES);
+    // // Circle
+    // gsi_circle(&gsi, 350.f, 170.f, 50.f, 20, 100, 150, 220, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    // gsi_circle(&gsi, 250.f, 170.f, 50.f, 20, 100, 150, 220, 255, GS_GRAPHICS_PRIMITIVE_LINES);
 
-    // Circle Sector
-    gsi_circle_sector(&gsi, 50.f, 150.f, 50.f, 0, 90, 32, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    gsi_circle_sector(&gsi, 150.f, 200.f, 50.f, 90, 270, 32, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_LINES);
+    // // Circle Sector
+    // gsi_circle_sector(&gsi, 50.f, 150.f, 50.f, 0, 90, 32, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    // gsi_circle_sector(&gsi, 150.f, 200.f, 50.f, 90, 270, 32, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_LINES);
 
     // Box (with texture)
-    gsi_depth_enabled(&gsi, true);
-    gsi_face_cull_enabled(&gsi, true);
-    gsi_camera3D(&gsi);
+    // gsi_depth_enabled(&gsi, true);
+    // gsi_face_cull_enabled(&gsi, true);
+    // gsi_camera3D(&gsi);
+    gsi_camera2D(&gsi);
     gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
     {
-        gsi_transf(&gsi, -2.f, -1.f, -5.f);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0005f, GS_ZAXIS);
+        // gsi_transf(&gsi, -2.f, -1.f, -5.f);
+        // gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
+        // gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0005f, GS_ZAXIS);
         gsi_texture(&gsi, tex.hndl);
-        gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
-        gsi_box(&gsi, 0.f, 0.f, 0.f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+        // gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
+        // gsi_box(&gsi, 0.f, 0.f, 0.f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    gsi_rectv(&gsi, gs_v2(300.f, 100.f), gs_v2(350.f, 50.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
         gsi_texture(&gsi, (gs_handle(gs_graphics_texture_t)){0});
     }
     gsi_pop_matrix(&gsi);
 
-    // Box (lines, no texture)
-    gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
-    {
-        gsi_transf(&gsi, 2.f, -1.f, -5.f);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0008f, GS_ZAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0009f, GS_XAXIS);
-        gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
-        gsi_box(&gsi, 0.f, 0.f, 0.f, 0.5f, 0.5f, 0.5f, 255, 200, 100, 255, GS_GRAPHICS_PRIMITIVE_LINES);
-    }
-    gsi_pop_matrix(&gsi);
+    // // Box (lines, no texture)
+    // gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
+    // {
+    //     gsi_transf(&gsi, 2.f, -1.f, -5.f);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0008f, GS_ZAXIS);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0009f, GS_XAXIS);
+    //     gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
+    //     gsi_box(&gsi, 0.f, 0.f, 0.f, 0.5f, 0.5f, 0.5f, 255, 200, 100, 255, GS_GRAPHICS_PRIMITIVE_LINES);
+    // }
+    // gsi_pop_matrix(&gsi);
     
-    // Sphere (triangles, no texture)
-    gsi_camera3D(&gsi);
-    gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
-    {
-        gsi_transf(&gsi, -2.f, -1.f, -5.f);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0005f, GS_ZAXIS);
-        gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
-        gsi_sphere(&gsi, 0.f, 0.f, 0.f, 1.0f, 255, 255, 255, 50, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    }
-    gsi_pop_matrix(&gsi);
+    // // Sphere (triangles, no texture)
+    // gsi_camera3D(&gsi);
+    // gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
+    // {
+    //     gsi_transf(&gsi, -2.f, -1.f, -5.f);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0005f, GS_ZAXIS);
+    //     gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
+    //     gsi_sphere(&gsi, 0.f, 0.f, 0.f, 1.0f, 255, 255, 255, 50, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
+    // }
+    // gsi_pop_matrix(&gsi);
 
-    // Sphere (lines)
-    gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
-    {
-        gsi_transf(&gsi, 2.f, -1.f, -5.f);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0008f, GS_ZAXIS);
-        gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0009f, GS_XAXIS);
-        gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
-        gsi_sphere(&gsi, 0.f, 0.f, 0.f, 1.0f, 255, 255, 255, 50, GS_GRAPHICS_PRIMITIVE_LINES);
-    }
-    gsi_pop_matrix(&gsi);
+    // // Sphere (lines)
+    // gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
+    // {
+    //     gsi_transf(&gsi, 2.f, -1.f, -5.f);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0008f, GS_ZAXIS);
+    //     gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.0009f, GS_XAXIS);
+    //     gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
+    //     gsi_sphere(&gsi, 0.f, 0.f, 0.f, 1.0f, 255, 255, 255, 50, GS_GRAPHICS_PRIMITIVE_LINES);
+    // }
+    // gsi_pop_matrix(&gsi);
 
-    // Text (custom and default fonts)
-    gsi_camera2D(&gsi);
-    gsi_defaults(&gsi);
-    gsi_text(&gsi, 410.f, 150.f, "Custom Font", &font, false, 200, 100, 50, 255);
-    gsi_text(&gsi, 450.f, 200.f, "Default Font", NULL, false, 50, 100, 255, 255);
+    // // Text (custom and default fonts)
+    // gsi_camera2D(&gsi);
+    // gsi_defaults(&gsi);
+    // gsi_text(&gsi, 410.f, 150.f, "Custom Font", &font, false, 200, 100, 50, 255);
+    // gsi_text(&gsi, 450.f, 200.f, "Default Font", NULL, false, 50, 100, 255, 255);
 
     gsi_render_pass_submit(&gsi, &cb, gs_color(10, 10, 10, 255));
 
