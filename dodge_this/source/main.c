43762f7a65c2bc1820be70a47d723f99729d5d3f
Jordan O'Leary <jdoleary@gmail.com>
2021-06-10 00:10:16 -0400

    clean

--- dodge_this/source/main.c
+++ dodge_this/source/main.c
@@ -61,59 +61,28 @@ void update()
     if (gs_platform_key_pressed(GS_KEYCODE_ESC)) gs_engine_quit();
 
 	gs_vec2 screenSize = gs_platform_window_sizev(gs_platform_main_window());
-        // gs_println("screen size: %f, %f", screenSize.x, screenSize.y);
+    // gs_println("screen size: %f, %f", screenSize.x, screenSize.y);
     camPos = gs_vec2_mul(heroPos, gs_v2(-1,-1));
     camPos = gs_vec2_add(camPos, gs_vec2_div(screenSize, gs_v2(2,2)));
     //     gs_println("camPos: %f, %f", camPos.x, camPos.y);
 
-
-    // gs_platform_t* platform = gs_engine_subsystem(platform);
-    // duration += platform->time.delta;
-        // gs_println("time: %f, %f", 1/platform->time.delta, duration);
-    // // Set up 2D camera for projection matrix
+    // Set up 2D camera for projection matrix
     gsi_camera2D(&gsi);
 
-    // // Rect
-    // gsi_rectv(&gsi, gs_v2(500.f, 50.f), gs_v2(600.f, 100.f), GS_COLOR_RED, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-
-    // // Triangle
-    // gsi_trianglev(&gsi, gs_v2(50.f, 50.f), gs_v2(100.f, 100.f), gs_v2(50.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    // gsi_trianglev(&gsi, gs_v2(200.f, 50.f), gs_v2(300.f, 100.f), gs_v2(200.f, 100.f), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_LINES);
-
-    // // Lines
-    // gsi_linev(&gsi, gs_v2(50.f, 20.f), gs_v2(500.f, 20.f), gs_color(0, 255, 0, 255));
-
     const gs_vec2 screenMousePos = gs_platform_mouse_positionv();
-        // gs_println("screen mouse pos: %f, %f", screenMousePos.x, screenMousePos.y);
-        // camPos.y = screenMousePos.y;
-        // camPos.x = screenMousePos.x;
     const gs_vec2 objectiveMousePos = gs_vec2_sub(screenMousePos, camPos);
         gs_println("obj mouse pos: %f, %f", objectiveMousePos.x, objectiveMousePos.y);
+    // Move hero to pointer
     moveToTarget(&heroPos, objectiveMousePos);
    
     gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
-            gsi_transf(&gsi, camPos.x, camPos.y, 0.f);
-            // gsi_transf(&gsi, 0.f, 200.f, 0.f);
-
-    gsi_push_matrix(&gsi, GSI_MATRIX_MODELVIEW);
-    {
-            // gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.001f, GS_YAXIS);
-    gsi_texture(&gsi, tex.hndl); // This is movab
-            gsi_transf(&gsi, 0.f, 0.f, 0.f);
-            gsi_rotatefv(&gsi, gs_platform_elapsed_time() * 0.005f, GS_ZAXIS);
-            // gsi_scalef(&gsi, 1.5f, 1.5f, 1.5f);
-            // gsi_box(&gsi, 0.f, 0.f, 0.f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-            gsi_rectv(&gsi, gs_v2(-size, -size), gs_v2(size, size), GS_COLOR_WHITE, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-    gsi_texture(&gsi, (gs_handle(gs_graphics_texture_t)){0});
-    }
-    gsi_pop_matrix(&gsi);
+    // Translate everything to inverse of camera to simulate "camera" motion
+    gsi_transf(&gsi, camPos.x, camPos.y, 0.f);
 
     gsi_rectv(&gsi, gs_v2(0.f, 0.f), screenSize, GS_COLOR_GREEN, GS_GRAPHICS_PRIMITIVE_LINES);
     gsi_circle(&gsi, heroPos.x, heroPos.y, 50.f, 20, 100, 150, 220, 255, GS_GRAPHICS_PRIMITIVE_TRIANGLES);
-        gs_println("hero pos: %f, %f", heroPos.x, heroPos.y);
+    // gs_println("hero pos: %f, %f", heroPos.x, heroPos.y);
     gsi_pop_matrix(&gsi);
-
- 
 
     gsi_render_pass_submit(&gsi, &cb, gs_color(10, 10, 10, 255));
 
